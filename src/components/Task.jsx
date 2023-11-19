e19a7f28147054b8eb7498931d7786a8222f8103
Emmanuel-Lima-dev <lima_kpo@hotmail.com>
2023-11-19 17:43:54 -0300

    second commit

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState} from "react";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
 import Title from "./Title";
@@ -6,7 +6,7 @@ import Title from "./Title";
 const Tasks = () => {
   //estado para almacenar las tareas
   const [tasks, setTasks] = useState([]);
-  const [idGenerator, setIdGenerator] = useState(1);
+  const [idGenerator, setIdGenerator] = useState(parseInt(localStorage.getItem('mockId')) || 1);
 
   // funcion para modificar estado completado de tarea
   const handleTaskCompleted = (taskId) => {
@@ -19,13 +19,17 @@ const Tasks = () => {
   //funcion para almacenar nueva tarea
   const handleTaskAdd = (taskName) => {
     if (taskName.length > 0) {
-      setIdGenerator(idGenerator + 1);
+      
+      localStorage.setItem('mockId', idGenerator)
       const newTask = {
         id: idGenerator,
         nameTask: taskName,
         completed: false,
       };
-      setTasks([...tasks, newTask]);
+      const result = [...tasks, newTask];
+      setTasks(result);
+      localStorage.setItem('dataTasks', JSON.stringify(result));
+      setIdGenerator(idGenerator + 1);
     }
   };
 
@@ -33,18 +37,25 @@ const Tasks = () => {
   const handleDeleteTask = (taskId) => {
     const newTaskArray = tasks.filter((task) => task.id !== taskId);
     setTasks(newTaskArray);
+    localStorage.setItem('dataTasks', JSON.stringify(newTaskArray))
   };
 
-  const firstRender = useRef(true);
+  // const firstRender = useRef(true);
+
+  useEffect(()=>{
+    localStorage.setItem('mockId', idGenerator);
+  }, [idGenerator])
 
   //useEffect que ejecutara un alert cada vez que cambien el estado en el almacenamiento de tareas
   useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      return;
-    }
-    
-  }, [tasks]);
+    // if (firstRender.current) {
+    //   firstRender.current = false;
+    //   return;
+    // }   
+    const StoredTasks =  JSON.parse(localStorage.getItem('dataTasks')) || [];
+    setTasks(StoredTasks);
+
+  }, []);
 
   return (
     <div>
