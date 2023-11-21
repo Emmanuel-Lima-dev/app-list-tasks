635d984faa6770d32ea8e939c23176ff0dc9b256
Emmanuel-Lima-dev <lima_kpo@hotmail.com>
2023-11-21 17:27:55 -0300

    alert de accion

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
 import Title from "./Title";
@@ -6,7 +6,11 @@ import Title from "./Title";
 const Tasks = () => {
   //estado para almacenar las tareas
   const [tasks, setTasks] = useState([]);
-  const [idGenerator, setIdGenerator] = useState(parseInt(localStorage.getItem('mockId')) || 1);
+  const [idGenerator, setIdGenerator] = useState(
+    parseInt(localStorage.getItem("mockId")) || 1
+  );
+  const [action, setAction] = useState(null);
+  const [viewAlert, setViewAlert] = useState(null)
 
   // funcion para modificar estado completado de tarea
   const handleTaskCompleted = (taskId) => {
@@ -14,14 +18,14 @@ const Tasks = () => {
       taskId === task.id ? { ...task, completed: !task.completed } : task
     );
     setTasks(updatedTasks);
-    localStorage.setItem('dataTasks', JSON.stringify(updatedTasks));
+    localStorage.setItem("dataTasks", JSON.stringify(updatedTasks));
+    setAction("edit");
   };
 
   //funcion para almacenar nueva tarea
   const handleTaskAdd = (taskName) => {
     if (taskName.length > 0) {
-      
-      localStorage.setItem('mockId', idGenerator)
+      localStorage.setItem("mockId", idGenerator);
       const newTask = {
         id: idGenerator,
         nameTask: taskName,
@@ -29,8 +33,9 @@ const Tasks = () => {
       };
       const result = [...tasks, newTask];
       setTasks(result);
-      localStorage.setItem('dataTasks', JSON.stringify(result));
+      localStorage.setItem("dataTasks", JSON.stringify(result));
       setIdGenerator(idGenerator + 1);
+      setAction("add");
     }
   };
 
@@ -38,28 +43,53 @@ const Tasks = () => {
   const handleDeleteTask = (taskId) => {
     const newTaskArray = tasks.filter((task) => task.id !== taskId);
     setTasks(newTaskArray);
-    localStorage.setItem('dataTasks', JSON.stringify(newTaskArray))
+    localStorage.setItem("dataTasks", JSON.stringify(newTaskArray));
+    setAction("delete");
   };
 
   // const firstRender = useRef(true);
 
-  useEffect(()=>{
-    localStorage.setItem('mockId', idGenerator);
-  }, [idGenerator])
-
-  //useEffect que ejecutara un alert cada vez que cambien el estado en el almacenamiento de tareas
   useEffect(() => {
-    // if (firstRender.current) {
-    //   firstRender.current = false;
-    //   return;
-    // }   
-    const StoredTasks =  JSON.parse(localStorage.getItem('dataTasks')) || [];
-    setTasks(StoredTasks);
+    localStorage.setItem("mockId", idGenerator);
+  }, [idGenerator]);
 
+  useEffect(() => {
+    switch (action) {
+      case "add":
+        setViewAlert("agregado");
+        break;
+      case "delete":
+        setViewAlert("borrado");
+        break;
+      case "edit":
+        setViewAlert("editado");
+        break;
+      default:
+        break;
+    }
+
+    setAction(null);
+  }, [tasks, action]);
+
+  useEffect(() => {
+    const StoredTasks = JSON.parse(localStorage.getItem("dataTasks")) || [];
+    setTasks(StoredTasks);
   }, []);
 
   return (
     <div>
+      {
+        viewAlert &&
+        (<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Holy!</strong> {viewAlert} con éxito...
+        <button
+          type="button"
+          class="btn-close"
+          
+          onClick={() => setViewAlert(null)}
+        ></button>
+      </div>)
+      }
       
       <Title title="Gestor de tareas" />
       <TaskForm setNewTask={handleTaskAdd} />
